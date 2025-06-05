d0548149c50e2dddf6c48261ba6f63a4577516b4
dedS3C5 <159559448+Estakuyo@users.noreply.github.com>
2025-06-05 22:38:55 +0800

    For production index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 require("dotenv").config()
 const cors = require('cors')
 const app = express()
+const path = require("path")
 const { mongoose } = require("mongoose")
 const cookieParser = require("cookie-parser")
 
